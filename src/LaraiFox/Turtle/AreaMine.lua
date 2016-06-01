b43060d3ca83edc1c5c95b2abd86d2daa8734dcd
LaraiFox <LaraiFox@hotmail.com>
2016-06-01 18:53:40 +0100

    # Initial commit of all files in the Eclipse (Lua) project folder.
    + Added TurtleUtils.lua containing extra utility functions as well as slight modifications to the default Turtle API.
        (see http://computercraft.info/wiki/Turtle_%28API%29 for details on the Turtle API)
    + Added AreaMine.lua, a program designed to make a turtle mine out a cuboid shaped area.
    
    Signed-off-by: LaraiFox <LaraiFox@hotmail.com>

---
+++ src/LaraiFox/Turtle/AreaMine.lua
@@ -0,0 +1,31 @@
+--- Description
+--
+-- @author Larai Fox ( http://pastebin.com/u/LaraiFox )
+-- @module AreaMine.lua
+
+local args = { ... }
+
+local TURTLE_DIRECTORY = "/LaraiFox/Turtle"
+assert(fs.isDir(TURTLE_DIRECTORY) and fs.exists(TURTLE_DIRECTORY), "Unable to locate required turtle files!\n" .. "Please download and run the turtle installer from http://pastebin.com/0anzz3sC to run this program.")
+
+dofile(TURTLE_DIRECTORY .. "/TurtleUtils.lua")
+
+--- @usage
+local usage = [[
+    Usage: AreaMine <width> <height> <length> [-t]
+    
+width   - The width of the area. Negative widths result in the turtle going right.
+height  - The height of the area. Negative heights result in the turtle going down.
+length  - The length of the area. Can not be negative.
+-t      - Causes the turtle to place torches as it mines.
+]]
+
+local function main()
+	--     Usage: AreaMine <width> <height> <length> [-t]
+	-- width	- The width of the area. Negative widths result in the turtle going right.
+	-- height	- The height of the area. Negative heights result in the turtle going down.
+	-- length	- The length of the area. Can not be negative.
+	-- -t		- Causes the turtle to place torches as it mines.
+end
+
+main()
